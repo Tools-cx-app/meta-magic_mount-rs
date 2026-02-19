abe7f80154f2c66fa73c8f58b290c80ffb773529
baaad <verybaaad@outlook.com>
2026-02-19 19:23:29 +0800

    feat: add webui shortcut button (#8)

--- build.rs
+++ build.rs
@@ -108,6 +108,7 @@ fn gen_module_prop(data: &CargoConfig) -> Result<()> {
         package.metadata.magic_mount_rs.update
     )?;
     writeln!(file, "description={}", package.description)?;
+    writeln!(file, "actionIcon=launcher.png")?;
     writeln!(file, "metamodule=1")?;
     Ok(())
 }
