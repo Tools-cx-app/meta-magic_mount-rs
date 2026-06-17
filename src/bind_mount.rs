61a1e1beee8107c3e916098e3ea31df9ae429dcb
Tools-cx-app <localhost.hutao@gmail.com>
2026-06-17 20:52:56 +0800

    bind: fix not mirror

--- src/bind_mount.rs
+++ src/bind_mount.rs
@@ -1,11 +1,14 @@
 // Copyright (C) 2026 Tools-cx-app <localhost.hutao@gmail.com>
 // SPDX-License-Identifier: Apache-2.0
 
-use std::fs;
+use std::path::Path;
 
 use rustix::mount::mount_bind;
 
-use crate::{errors::Result, parser::COMMAND_LIST, utils::ksucalls::send_unmountable};
+use crate::{
+    errors::Result, magic_mount::utils::mount_mirror, parser::COMMAND_LIST,
+    utils::ksucalls::send_unmountable,
+};
 
 pub fn bind_mount(umount: bool) -> Result<()> {
     let bind_mount_list: Vec<_> = COMMAND_LIST
@@ -24,12 +27,29 @@ pub fn bind_mount(umount: bool) -> Result<()> {
     for (s, t) in bind_mount_list {
         log::debug!("bind mount: {s} -> {t}");
 
-        if fs::exists(&s).is_err() || fs::exists(&t).is_err() {
+        let source = Path::new(&s);
+        let target = Path::new(&t);
+        let workdir = tempfile::Builder::new().tempdir()?;
+        let mut has_mirror = false;
+
+        if !target.exists()
+            && let Some(parent) = target.parent()
+        {
+            for entry in parent.read_dir()?.flatten() {
+                mount_mirror(parent, workdir.path(), &entry)?;
+                has_mirror = true;
+            }
+        }
+        if !source.exists() || (!target.exists() && !has_mirror) {
             log::error!("source/target isn't existed, skip!!");
             continue;
         }
 
-        mount_bind(&s, &t)?;
+        if has_mirror {
+            mount_bind(workdir.path(), &t)?;
+        } else {
+            mount_bind(&s, &t)?;
+        }
         if umount {
             send_unmountable(&t);
         }
