786c760cb709afc3bc962a25bd2272c11df738a5
YC酱luyancib <luyancib@qq.com>
2026-06-18 21:38:37 +0800

    fix build

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
+  build: {
+    outDir: "../module/webroot",
+    target: "esnext",
+  },
   plugins: [vue()],
 })
