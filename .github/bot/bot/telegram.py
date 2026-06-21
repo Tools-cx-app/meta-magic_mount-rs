9c4e2f8e7835e418a241c790d92b1367cddc6fb1
喵喵机 <2438926613@qq.com>
2026-06-21 16:08:57 +0800

    bot: rebuild inputmediadocument

--- .github/bot/bot/telegram.py
+++ .github/bot/bot/telegram.py
@@ -20,7 +20,8 @@ async def post(msg: str, files: list[str], parse_mode: str):
         else:
             logger.info(f"Sending {len(files)} files with {parse_mode} caption:\n{msg}")
             medias = [telegram.InputMediaDocument(open(f, "rb")) for f in files]
-            medias[-1].caption = msg
-            medias[-1].parse_mode = parse_mode
+            medias[-1] = telegram.InputMediaDocument(
+                open(files[-1], "rb"), msg, parse_mode
+            )
             await bot.send_media_group(settings.chat_id, medias)
     logger.info("Successfully posted to Telegram")
